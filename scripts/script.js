a896dd3b21c76248b17eb3dfb277dab7e7df7cff
torezani89 <torezani89@outlook.com>
2021-10-10 23:49:16 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@
     import {criaCelulas} from "./loadTabela.js";
     import {preparaDeposito, preparaSaque, transferir, destinatariosDeposito, 
         clientesSaque} from "./transacoes.js";
+    import {sortTable} from "./ordenaTabela.js";
+    // import {criaCliente} from "./criaCliente.js"
 
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< CARREGAR TABELA >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -20,4 +22,48 @@ botaoDepositar.addEventListener('click', preparaDeposito);
 
 botaoSacar.addEventListener('click', preparaSaque);
 
-botaoTranf.addEventListener('click', transferir);   
+botaoTranf.addEventListener('click', transferir); 
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< EVENTOS ORDENAR TABELA >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+let myTable = document.querySelector('#tbody');
+let thNome = document.querySelector('#th-nome');
+let thSaldo = document.querySelector('#th-saldo');
+
+let ord = false;
+
+thNome.addEventListener('dblclick', function () {
+  
+  thSaldo.className = "";
+  thNome.className = "";
+
+  if (!ord) {
+    sortTable(myTable, 'asc', 0);
+    thNome.classList.add("ascendente");
+    ord = true;
+  } else {
+    sortTable(myTable, 'desc', 0);
+    thNome.classList.add("descendente");
+    ord = false;
+  }
+
+})
+
+thSaldo.addEventListener('dblclick', function () {
+
+  thNome.className = "";
+  thSaldo.className = "";
+
+  if (!ord) {
+    sortTable(myTable, 'asc', 3);
+    thSaldo.classList.add('ascendente');
+    ord = true;
+  } else {
+    sortTable(myTable, 'desc', 3);
+    thSaldo.classList.add('descendente');
+    ord = false;
+  }
+
+})
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< EVENTO BOTAO INCLUIR CLIENTE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
