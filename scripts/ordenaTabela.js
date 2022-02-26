722c7aef45803fa5b6bc1e670527be6466d9e76b
torezani89 <torezani89@outlook.com>
2022-02-26 15:27:16 -0300

    Update ordenaTabela.js

--- scripts/ordenaTabela.js
+++ scripts/ordenaTabela.js
@@ -1,3 +1,12 @@
+function txt2number (txt) {
+    var num = txt
+    num = num.replace(/[R$&nbsp;]/g, '')
+    num = num.replace(/\./g, '')
+    num = num.replace(',', '.')
+    num = parseFloat(num)
+    return num
+}
+
 function sortTable(table, dir, n) {
 
     var rows;
@@ -5,7 +14,7 @@ function sortTable(table, dir, n) {
     var x;
     var y;
     var shouldSwitch;
-    var switchcount = 0;
+    // var switchcount = 0;
     var switching = true;
 
     /*Faça um loop que continuará até nenhuma troca ser feita:*/
@@ -15,31 +24,29 @@ function sortTable(table, dir, n) {
         switching = false;
 
         rows = table.rows;
-        
-        /*Faça um loop por todas as linhas da tabela (exceto o primeiro, que contém cabeçalhos da tabela):*/
+
+        /*Faça um loop por todas as linhas do t-body:*/
         for (i = 0; i < (rows.length - 1); i++) {
             //comece dizendo que não deve haver alternância/troca:
             shouldSwitch = false;
             /*Obtenha os dois elementos que você deseja comparar, um da linha atual e o outro da próxima:*/
-            if (n == 3) {
-                x = rows[i].getElementsByTagName("TD")[n].innerHTML;
-                y = rows[i + 1].getElementsByTagName("TD")[n].innerHTML;
+            if (n == 3) { //Se n for a coluna saldo (comparação de numbers)
+                let txtX = rows[i].getElementsByTagName("TD")[n].innerHTML
+                let txtY = rows[i + 1].getElementsByTagName("TD")[n].innerHTML
+                x = txt2number(txtX)
+                y = txt2number(txtY)
             } else {
                 x = rows[i].getElementsByTagName("TD")[n].innerHTML.toLowerCase();
                 y = rows[i + 1].getElementsByTagName("TD")[n].innerHTML.toLowerCase();
             }
 
             /*verifique se as duas linhas devem mudar de lugar, com base na direção, asc ou desc:*/
-            if (dir == "asc") {
-            if (x > y) {
+            if (dir == "asc" && x > y) {
                 shouldSwitch = true;
                 break; //Pare o loop.
-            }
-            } else if (dir == "desc") {
-            if (x < y) {
+            } else if (dir == "desc" && x < y) {            
                 shouldSwitch = true;
                 break; //Pare o loop
-            }
             }
         }
 
@@ -48,7 +55,7 @@ function sortTable(table, dir, n) {
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
             switching = true;
             //Cada vez que uma troca for concluída, aumente essa contagem em 1:
-            switchcount++;
+            // switchcount++;
         }
         // else {
         //     /*Se nenhuma mudança deve ser feita (shouldSwitch == false) e a direção for "asc",
@@ -60,5 +67,49 @@ function sortTable(table, dir, n) {
         // }
     }
   }
+        
+        /*
+        SUGESTÃO ALTERNATIVA ARTHUR - FORMA MAIS MODERNA:
 
-  export {sortTable}
+        var db = JSON.parse(localStorage.getItem('db'))
+
+        function saveDB (db) {
+            localStorage.setItem('db', JSON.stringify(db))
+        }
+
+        var linhas = [
+            { nome: '', saldo: 100 },
+            { nome: '', saldo: 100 },
+            { nome: '', saldo: 100 },
+            { nome: '', saldo: 100 },
+        ]
+        function ordenaTabela (linhas) {
+            linhas.sort((a, b) => {
+                return a.saldo > b.saldo 
+            })
+            render()
+        }
+        function render () {
+            var table = document.getElementById('idTabela')
+            table.innerHTML = '<tr><td>Loading...</td></tr>'
+
+            var content = ''
+            for (linha of linhas) {
+                content += `
+                    <tr>
+                        <td>${linha.nome}</td>
+                        <td>${linha.saldo}</td>
+                    </tr>
+                `
+            }
+
+            table.innerHTML = content
+        }
+
+        parseFloat('50.010,00')
+        linhas[2].saldo = 50010.00
+        `<td>${formatMoney(linha.saldo)}</td>` -> <td>50.010,00</td>
+
+        */
+
+export {sortTable}
